20de2351f1b7c90476872e28da7d387e084ad6d4
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-03 18:26:07 -0300

    commit 3

--- curso-php/screen-match/index.php
+++ curso-php/screen-match/index.php
@@ -1,13 +1,17 @@
 <?php
 
+require __DIR__ . "/src/Modelo/Genero.php";
+require __DIR__ . "/src/Modelo/Titulo.php";
 require __DIR__ . "/src/Modelo/Filme.php";
+require __DIR__ . "/src/Modelo/Serie.php";
 
 echo "Bem-vindo(a) ao ScreenMatch\n";
 
 $filme = new Filme(
   'Thor - Ragnarok',
   2017,
-  'super-heroi'
+  Genero::SuperHeroi,
+  130
 );
 
 $filme->avalia(10);
@@ -19,3 +23,8 @@ var_dump($filme);
 
 echo $filme->media() . PHP_EOL;
 echo $filme->anoLancamento . PHP_EOL;
+
+$serie = new Serie('Lost', 2007, Genero::Drama, 10, 20, 30);
+echo $serie->anoLancamento . "\n";
+$serie->avalia(8);
+echo $serie->media() . "\n";
