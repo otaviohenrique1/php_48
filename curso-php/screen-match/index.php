c5379a49af90468238ef88dd85beab4047f7d72e
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-03 15:38:33 -0300

    commit 2

--- curso-php/screen-match/index.php
+++ curso-php/screen-match/index.php
@@ -1,116 +1,21 @@
 <?php
 
-require_once __DIR__ . '/src/funcoes.php';
+require __DIR__ . "/src/Modelo/Filme.php";
 
-// php index.php 10 5 6 8 9.5
+echo "Bem-vindo(a) ao ScreenMatch\n";
 
-echo "Bem-vindo(a) ao screen match!" . PHP_EOL;
+$filme = new Filme(
+  'Thor - Ragnarok',
+  2017,
+  'super-heroi'
+);
 
-$nomeFilme = "Top Gun - Maverick";
-// $nomeFilme = "Thor: Ragnarok";
-// $nomeFilme = "Se beber não case";
+$filme->avalia(10);
+$filme->avalia(10);
+$filme->avalia(5);
+$filme->avalia(5);
 
-$anoLancamento = 2022;
-// $anoLancamento =  $argv[1] ?? 2024;
-// $notaFilme = 8.8;
+var_dump($filme);
 
-// $somaDeNotas = 9;
-// $somaDeNotas += 6;
-// $somaDeNotas += 8;
-// $somaDeNotas += 7.5;
-// $somaDeNotas += 5;
-// $notaFilme = $somaDeNotas / 5;
-
-$quantidadeDeNotas = $argc - 1;
-$somaDeNotas = 0;
-
-for ($i=0; $i < $argc; $i++) { 
-  $somaDeNotas += (float) $argv[$i];
-}
-
-// $somaDeNotas += $argv[1];
-// $somaDeNotas += $argv[2];
-// $somaDeNotas += $argv[3];
-
-$notaFilme = $somaDeNotas / $quantidadeDeNotas;
-
-$planoPrime = true;
-$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
-
-// $nomeFilme = $argv[2];
-// $notaFilme =  $argv[3];
-
-echo "Nome do filme" . $nomeFilme . PHP_EOL;
-echo "Nota do filme" . $notaFilme . PHP_EOL;
-echo "Ano de lançamento" . $anoLancamento . PHP_EOL;
-
-exibeMesagemLancamento($anoLancamento);
-
-$genero = match($nomeFilme) {
-  "Top Gun - Maverick" => "ação",
-  "Se beber não case" => "comédia",
-  "Thor: Ragnarok" => "super-herói",
-  default => "gênero desconhecido",
-};
-
-echo "O gênero do filme é: $genero" .  PHP_EOL;
-echo $argc . PHP_EOL;
-
-// $filme = [
-//   "nome" => "Thor: Ragnarok",
-//   "ano" => 2021,
-//   "nota" => 7.8,
-//   "genero" => "super-herói"
-// ];
-
-$filme = criaFilme(nome: "Thor: Ragnarok", anoLancamento: 2021, nota: 7.8, genero: "super-herói");
-
-echo $filme['nome'] . PHP_EOL;
-
-// $notasParaOFilme = [
-//   10, 8, 9, 7.5, 5, 6.8
-// ];
-
-// $notasParaOFilme2 = array(
-//   10, 8, 9, 7.5, 5, 6.8
-// );
-
-// $a = array(
-//   0 => "a",
-//   1 => "b",
-//   2 => "c",
-//   3 => "d",
-//   4 => "e",
-//   5 => "f",
-// );
-
-$notas = [];
-
-for ($i=1; $i < $argc; $i++) { 
-  $notas[] = (float) $argv[$i];
-}
-
-var_dump($notas);
-
-$somaDeNotas = 0;
-// foreach ($notas as $nota) { 
-//   $somaDeNotas += $nota;
-// }
-$notaFilme = array_sum($notas) / $quantidadeDeNotas;
-
-echo $notaFilme . PHP_EOL;
-
-var_dump($notas);
-sort($notas);
-var_dump($notas);
-$menorNota = min($notas);
-var_dump($menorNota);
-var_dump($filme['nome']);
-$posicaoDoisPontos = strpos($filme['nome'], ':');
-var_dump($posicaoDoisPontos);
-var_dump(substr($filme['nome'], 0, $posicaoDoisPontos));
-
-$filmeComoStringJson = json_encode($filme);
-echo $filmeComoStringJson;
-// var_dump(json_decode($filmeComoStringJson, true));
-file_put_contents(__DIR__ . '/filme.json', $filmeComoStringJson);
+echo $filme->media() . PHP_EOL;
+echo $filme->anoLancamento . PHP_EOL;
--- curso-php/screen-match/src/Modelo/Filme.php
+++ curso-php/screen-match/src/Modelo/Filme.php
@@ -2,8 +2,55 @@
 
 class Filme
 {
-  public string $nome;
-  public int $anoLancamento;
-  public string $genero;
-  public string $nota;
+  private array $notas;
+
+  public function __construct(
+    public readonly string $nome,
+    public readonly int $anoLancamento,
+    public readonly string $genero
+  ) {
+    $this->notas = [];
+  }
+
+  public function avalia(float $nota): void
+  {
+    $this->notas[] = $nota;
+  }
+
+  public function media(): float
+  {
+    $somaNotas = array_sum($this->notas);
+    $quantidadeNotas = count($this->notas);
+    return $somaNotas / $quantidadeNotas;
+  }
+
+  // public function nome(): string
+  // {
+  //   return $this->nome;
+  // }
+  
+  // public function anoLancamento(): int
+  // {
+  //   return $this->anoLancamento;
+  // }
+  
+  // public function genero(): string
+  // {
+  //   return $this->genero;
+  // }
+
+  // public function defineNome(string $nome)
+  // {
+  //   $this->nome = $nome;
+  // }
+  
+  // public function defineAnoLancamento(int $anoLancamento)
+  // {
+  //   $this->anoLancamento = $anoLancamento;
+  // }
+  
+  // public function defineGenero(string $genero)
+  // {
+  //   $this->genero = $genero;
+  // }
 }
