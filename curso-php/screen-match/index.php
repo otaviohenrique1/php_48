e5cd16ece30f6a3a371505cf90d604688d928c00
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-03 18:31:15 -0300

    commit 5

--- curso-php/screen-match/index.php
+++ curso-php/screen-match/index.php
@@ -4,6 +4,7 @@ require __DIR__ . "/src/Modelo/Genero.php";
 require __DIR__ . "/src/Modelo/Titulo.php";
 require __DIR__ . "/src/Modelo/Filme.php";
 require __DIR__ . "/src/Modelo/Serie.php";
+require __DIR__ . "/src/Calculos/CalculadoraDeMaratona.php";
 
 echo "Bem-vindo(a) ao ScreenMatch\n";
 
@@ -28,3 +29,9 @@ $serie = new Serie('Lost', 2007, Genero::Drama, 10, 20, 30);
 echo $serie->anoLancamento . "\n";
 $serie->avalia(8);
 echo $serie->media() . "\n";
+
+$calculadora = new CalculadoraDeMaratona();
+$calculadora->inclui($filme);
+$calculadora->inclui($serie);
+$duracao = $calculadora->duracao();
+echo "Para essa maratona, você precisa de $duracao minutos";
